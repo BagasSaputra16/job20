31433fcf9b97d7f5df5f314beb0b9b785a20feb3
ceng22 <riska@gmail.com>
2022-12-14 07:44:10 +0700

    first commit

---
+++ app/src/main/java/com/app/project2_alarm_jam/bagasjob20/Kelas.kt
@@ -0,0 +1,7 @@
+package com.app.project2_alarm_jam.bagasjob20
+
+
+data class Kelas (
+    val nama: String?,
+    val nis: String?
+)
---
+++ app/src/main/java/com/app/project2_alarm_jam/bagasjob20/list.kt
@@ -0,0 +1,11 @@
+package com.app.project2_alarm_jam.bagasjob20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
